4d6ef6bafccd6933a16d811fbe226c909abc3baf
athaya1904 <146912498+athaya1904@users.noreply.github.com>
2023-12-27 16:05:40 +0700

    Create fungsitipedata.c

---
+++ Tugas02_PetaniKode/fungsitipedata.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int usia = 19;
+    float berat = 55.12;
+    double tinggi = 180.10;
+    char jenis_kelamin = 'L';
+
+    printf("Usia: %d tahun\n", usia);
+    printf("Tinggi: %.2f cm\n", tinggi);
+    printf("Berat: %.2f Kg\n", berat);
+    printf("Jenis kelamin: %c\n", jenis_kelamin);
+
+    return 0;
+}
