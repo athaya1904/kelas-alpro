645910058371f7838376a82cf129049ee6a4b5c0
athaya1904 <146912498+athaya1904@users.noreply.github.com>
2023-12-27 15:58:56 +0700

    Create fungsiaritmatika.c

---
+++ Tugas02_PetaniKode/fungsiaritmatika.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(){
+
+    int x, y;
+
+    printf("Inputkan nilai x: ");
+    scanf("%d", &x);
+
+    printf("Inputkan nilai y: ");
+    scanf("%d", &y);
+
+    printf("Hasil x+y: %i\n", x+y);
+    printf("Hasil x-y: %i\n", x-y);
+    printf("Hasil x*y: %i\n", x*y);
+    printf("Hasil x/y: %i\n", x/y);
+
+}
