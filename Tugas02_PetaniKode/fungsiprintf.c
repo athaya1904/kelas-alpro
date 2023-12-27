52e7b584bd92a4a0368c267342338e4efc93107e
athaya1904 <146912498+athaya1904@users.noreply.github.com>
2023-12-27 16:04:45 +0700

    Update fungsiprintf.c

--- Tugas02_PetaniKode/fungsiprintf.c
+++ Tugas02_PetaniKode/fungsiprintf.c
@@ -2,7 +2,7 @@
 
 int main(){
     printf("Halo teman-teman\n");
-    printf("Nama saya %s\n", "Alfonso");
+    printf("Nama saya %s\n", "Ghefira!");
     printf("Usia saya %d\n", 19);
     return 0;
 }
