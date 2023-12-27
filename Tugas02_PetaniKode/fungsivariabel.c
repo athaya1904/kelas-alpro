dc81a1ba11a825cc3682c609e26705d1265d82af
athaya1904 <146912498+athaya1904@users.noreply.github.com>
2023-12-27 16:06:22 +0700

    Create fungsivariabel.c

---
+++ Tugas02_PetaniKode/fungsivariabel.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int berat;
+    int tinggi = 165;
+    berat = 63;
+
+    printf("Berat badan saya adalah %d kg dan tinggi %d cm", berat, tinggi);
+
+    return 0;
+}
